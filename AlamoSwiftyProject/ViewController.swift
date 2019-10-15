cdbd93620ca42d12c49bdc115333f07ed12c720b
Lachlan <s3722123@student.rmit.edu.au>
2019-10-15 12:35:07 +1100

    Initial Commit

---
+++ AlamoSwiftyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamoSwiftyProject
+//
+//  Created by Lachlan  Wilson on 15/10/19.
+//  Copyright © 2019 Lachlan  Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
